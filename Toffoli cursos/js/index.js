398ea101fdef62ec6be5e3e6f180d4eaddac2048
gepassos <gustavo.escuderopassos@gmail.com>
2020-06-23 14:54:22 -0400

    ajustes add Class

--- Toffoli cursos/js/index.js
+++ Toffoli cursos/js/index.js
@@ -4,7 +4,7 @@ const infoImgsPokemons = [
     { name: 'Pikachu', img: '../Imgs/Pikachu.jpg', info: 'His nature is to store up electricity. Forests where nests of Pikachu live are dangerous, since the trees are so often struck by lightning.' },
     { name: 'Raikou', img: '../Imgs/raikou.jpg', info: 'Raikou embodies the speed of lightning. The roars of this Pokémon send shock waves shuddering through the air and shake the ground as if lightning bolts had come crashing down.' },
     { name: 'Gengar', img: '../Imgs/gengar.png', info: 'You can hear tales told all over the world about how Gengar will pay a visit to children who are naughty.' },
-    { name: 'Charmander', img: './Imgs/Charmander.png', info: 'The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely.' },
+    { name: 'Charmander', img: '../Imgs/Charmander.png', info: 'The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely.' },
     { name: 'Charmeleon', img: '../Imgs/Charmeleon.png', info: 'Charmeleon mercilessly destroys its foes using its sharp claws. If it encounters a strong foe, it turns aggressive. In this excited state, the flame at the tip of its tail flares with a bluish white color..' },
     { name: 'Charizard', img: '../Imgs/Charizard.png', info: 'Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.' },
     { name: 'Squirtle', img: '../Imgs/Squirtle.png', info: 'Squirtles shell is not merely used for protection.' }
@@ -40,8 +40,26 @@ function setInfoImg() {
 }
 
 
-function adicionarPokemon() {
 
 
+function removeClassNav() {
+    let classRemover = document.querySelectorAll('.active')
+    for (let i = 0; i < classRemover.length; i++) {
+        classRemover[i].classList.remove('active')
+    }
 }
+
+
+
+function addClassNav() {
+    let classAdd = document.querySelectorAll('.nav-link')
+    let urlPage = window.location.pathname
+    tagA = document.getElementsByTagName('a')
+    for (let i = 0; i < classAdd.length; i++) {
+        if (urlPage) {
+            classAdd[i].classList.add('active')
+        }
+    }
+}
+
 
